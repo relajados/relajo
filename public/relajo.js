fff84304f9fb840dc43609d4aa0cc618490c1476
Aythami Santana <aythami.santana@aentos.es>
2013-08-04 13:12:09 +0100

    Extracted visual stuff from relajo

--- public/relajo.js
+++ public/relajo.js
@@ -67,62 +67,6 @@ var relajo = function () {
     });
 
     relajo.socket = socket;
-
-    if (chrome.browserAction) {
-        chrome.browserAction.onClicked.addListener(function (tab) {
-            relajo.play("http://www.w3schools.com/tags/horse.ogg");
-        });
-    };
-
-    relajo.play = function (media) {
-        var node = document.getElementsByTagName('video')[0];
-        if (node) {
-            node.parentNode.removeChild(node);
-        }
-        node = document.createElement('video');
-        node.src = media;
-
-        document.body.appendChild(node);
-
-        node.play(media);
-    }
-
-    relajo.say = function (message) {
-        var audio = document.createElement('audio');
-        audio.setAttribute("autoplay", "autoplay");
-        audio.src = "http://translate.google.com/translate_tts?tl=es&q=" + message;
-    }
-
-    relajo.fun_with = function (param) {
-        var url = 'https://dl.dropboxusercontent.com/u/66061646/dashboard_videos/'
-        if (param == 'sangre') {
-            relajo.play(url + "sangre_azteca.mp3");
-        } else {
-            relajo.play(url + "trambolico.mp3");
-        }
-    }
-
-    var div = document.getElementById("relajo");
-    if (div) {
-        display("Relajo already enabled!");
-    } else {
-        div = document.createElement("relajo");
-        div.id = "relajo";
-        div.style.position = "fixed";
-        div.style.zIndex = "100";
-        div.style.width = "100%";
-        div.style.background = "black";
-
-        var button = document.createElement("button");
-        button.setAttribute("onclick", "relajo.socket.emit('eval', 'relajo.fun_with(1);');");
-
-        var text = document.createTextNode("trambolico");
-        button.appendChild(text);
-
-        div.appendChild(button);
-
-        document.body.insertBefore(div, document.body.firstChild);
-    }
 }
 
 JavaScript.load("https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js", function(){
