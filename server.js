6cc4d996d95c4d7935ff4058eebef56fc0777331
Dashboard <aythami.santana@aentos.es>
2013-09-27 11:33:01 +0100

    Added stop message to kill mplayer in the dashboard

--- server.js
+++ server.js
@@ -4,35 +4,41 @@ var express = require("express");
 var app = express();
 var io = require('socket.io').listen(app.listen(port));
 
-io.enable('browser client minification');  // send minified client
-io.enable('browser client etag');          // apply etag caching logic based on version number
-io.enable('browser client gzip');          // gzip the file
-io.set('log level', 1);                    // reduce logging
-
-console.log("Listening on port " + port);
+io.enable('browser client minification');
+io.enable('browser client etag');
+io.enable('browser client gzip');
+io.set('log level', 1);
 
 app.use(express.static(__dirname + '/public'));
-
+/*
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
 });
-
+*/
 io.sockets.on('connection', function (socket) {
-  console.log('connection: ' + socket);
+  console.log('connection' + socket.id);
   io.sockets.emit('message', 'connection ' + socket.id);
 
   socket.on('message', function(message, callback) {
-    console.log('message: ' + message);
+    console.log('message ' + message + ' callback ' + callback);
     io.sockets.emit('message', message);
   });
 
+  socket.on('stop', function(json) {
+    console.log('stop ' + json);
+    io.sockets.emit('stop', json);
+  });
+
   socket.on('eval', function(json) {
-    console.log('eval: ' + json);
+    console.log('eval ' + json);
     io.sockets.emit('eval', json);
   });
 
-  socket.on('disconnect', function () {
-    io.sockets.emit('user disconnected');
+  socket.on('disconnect', function (socket) {
+    console.log('disconnect' + socket.id);
+    io.sockets.emit('message', 'disconnect ' + socket.id);
   });
 });
+
+console.log("Listening on port " + port);
